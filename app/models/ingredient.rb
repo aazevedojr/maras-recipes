c9b15d34ec673bde813723c0995d7bbddd0fea2f
Ashley Teow <ashley.sw.teow@gmail.com>
2017-08-30 11:56:04 -0700

    Add validations and associations to models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  has_many :measurements
+  has_many :recipes, through: :measurements
+  has_many :categories, through: :recipes
+
+  validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  belongs_to :category
+  belongs_to :creator, class_name: :User
+  has_many :measurements
+  has_many :ingredients, through: :measurements
+
+  validates :name, :prep_time, :directions, :difficulty, presence: true
 end
