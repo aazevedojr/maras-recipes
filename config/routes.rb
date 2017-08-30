9115237f5dd30a1f324c57a654c7663470326cd2
Ashley Teow <ashley.sw.teow@gmail.com>
2017-08-30 15:10:01 -0700

    Add rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'categories#index'
+  resources :categories, only: [:index, :show]
+  resources :recipes do
+    resources :ingredients
+  end
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#delete'
+
+  get 'register', to: 'users#new'
+  post 'register', to: 'users#create'
 end
